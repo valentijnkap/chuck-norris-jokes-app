ce013e8272e9e73d0828787425786c14d409e6d4
Valentijn Kap <info@valentijnkap.nl>
2021-02-13 22:55:44 +0100

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,26 @@
+let mix = require('laravel-mix')
+
+mix
+  .options({
+    processCssUrls: false,
+    terser: {
+      extractComments: false,
+    },
+    postCss: [
+      require('autoprefixer')({
+        grid: true,
+      }),
+    ],
+  })
+  .sourceMaps(true, 'source-map')
+
+mix.sass(`./sass/main.scss`, `./dist/main.css`)
+mix.js(`./javascript/app.js`, `./dist/app.js`)
+
+mix.browserSync({
+  proxy: false,
+  server: {
+    baseDir: './',
+  },
+  files: ['*.js', '*.scss', '*.html'],
+})
